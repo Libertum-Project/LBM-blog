ba47eddc76f02c93da6e949d928500cc386c4f05
nikiburgos <burgosveganicole@gmail.com>
2024-03-21 15:51:48 -0300

    share post

--- app/(Home)/[...slug]/page.tsx
+++ app/(Home)/[...slug]/page.tsx
@@ -1,6 +1,7 @@
 import { posts } from "#site/content";
 import { MDXContent } from "@/components/ui/mdx-components";
 import { notFound } from "next/navigation";
+import Share from "@/components/Share";
 
 import "@/styles/mdx.css";
 import { Metadata } from "next";
@@ -75,15 +76,15 @@ export default async function PostPage({ params }: PostPageProps) {
     <article className="container py-6 prose max-w-3xl mx-auto md:max-w-[75rem] pt-32 pb-12 md:justify-between gap-20">
        <div className="hidden md:block absolute inset-0 bg-gradient-to-b from-[#0E0E1E] to-[#000041]" style={{ height: '5rem', zIndex: '-1' }} /> 
           <h1 className="mb-4 text-[#0E0E1E] font-space_grotesk font-bold">{post.title}</h1>
-        <div className="flex gap-2 mb-2">
-            {post.tags?.map((tag) => (
-            <Tag tag={tag} key={tag} />
-            ))}
-        </div>
-
       {post.description ? (
         <p className="text-xl mt-0 md:mt-10 text-muted-foreground text-[#0E0E1E]">{post.description}</p>
       ) : null}
+      <div className="flex gap-2 mb-2">
+        {post.tags?.map((tag) => (
+          <Tag tag={tag} key={tag} />
+        ))}
+      </div>
+        <Share description={"this is a basic share page"}/>
       <hr className="my-4" />
       <MDXContent code={post.body} />
     </article>
