1ed26901ed55aa088777db5b6e68f35c0841d792
Mateusz Wuzik <wuzikm@gmail.com>
2024-05-24 15:38:39 +0200

    chore: postcss cjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { PropsWithChildren } from 'react';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -6,13 +7,13 @@ import { siteConfig } from '@/config/site';
 export const metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
-  metadataBase: new URL(siteConfig.url),
+  metadataBase: new URL(siteConfig.url)
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={montserrat.className} id='body'>
+      <body className={montserrat.className} id="body">
         {children}
       </body>
     </html>
